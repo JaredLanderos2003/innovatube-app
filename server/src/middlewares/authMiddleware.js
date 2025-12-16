67ca4b73b595d83e67151eaf400861057caf662d
Jared Landeros <jaredlanderos@MacBook-Pro-de-Jared.local>
2025-12-16 00:51:56 -0600

    Actualizando middleware para leer token desde cookies

--- server/src/middlewares/authMiddleware.js
+++ server/src/middlewares/authMiddleware.js
@@ -2,17 +2,22 @@ import jwt from 'jsonwebtoken';
 
 export const verificarToken = (req, res, next) => {
   try {
-    const token = req.headers.authorization?.split(' ')[1];
+    let token = req.cookies.token;
+
+    if (!token && req.headers.authorization) {
+        token = req.headers.authorization.split(' ')[1];
+    }
 
     if (!token) {
       return res.status(401).json({ mensaje: 'No tienes permiso. Inicia sesión.' });
     }
-
+    
     const decoded = jwt.verify(token, process.env.JWT_SECRET || 'secreto_temporal');
     req.usuarioId = decoded.id;
     next();
     
   } catch (error) {
+    console.log("Error verificando token:", error.message);
     return res.status(401).json({ mensaje: 'Token inválido o expirado.' });
   }
 };
